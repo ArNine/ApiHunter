7266b5c5e803a3b11945ecf91905092c81006418
Ar <851332120@qq.com>
2024-07-02 23:52:19 +0800

    init

---
+++ ApiHunter.py
@@ -0,0 +1,19 @@
+import requests
+
+
+class ApiHunter:
+
+    def __int__(self, request_pool, thread_num):
+        pass
+
+    def produce(self, request_body: dict):
+        pass
+
+    def request(self):
+        pass
+
+    def response(self):
+        pass
+
+
+
